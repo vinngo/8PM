6b46e8cf364b654a0a06bf61381aab9ec218ce2a
vinngo <ngovincent9593@gmail.com>
2025-10-05 19:02:01 -0700

    Initial Commit

---
+++ 8PM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  8PM
+//
+//  Created by Vincent Ngo on 10/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 8PM/_PMApp.swift
@@ -0,0 +1,17 @@
+//
+//  _PMApp.swift
+//  8PM
+//
+//  Created by Vincent Ngo on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct _PMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
